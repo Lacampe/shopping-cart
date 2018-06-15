a7c5f9f1b5aa5e62f75fbc6ee7245d69d503d146
Romain Campech <romain.campech@city.ac.uk>
2018-06-15 15:08:57 +0100

    Refacto routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
-var csrf = require('csurf');
-var passport = require('passport');
 
 var Product = require('../models/product');
-
-var csrfProtection = csrf();
-router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -18,32 +13,6 @@ router.get('/', function(req, res, next) {
     }
     res.render('shop/index', { products: productChunks });
   });
-});
-
-router.get('/user/signup', function(req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signup', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
-});
-
-router.post('/user/signup', passport.authenticate('local.signup', {
-  successRedirect: 'profile',
-  failureRedirect: 'signup',
-  failureFlash: true
-}));
-
-router.get('/user/signin', function(req, res, next) {
-  var messages = req.flash('error');
-  res.render('user/signin', { csrfToken: req.csrfToken(), messages: messages, hasErrors: messages.length > 0 });
-});
-
-router.post('/user/signin', passport.authenticate('local.signin', {
-  successRedirect: 'profile',
-  failureRedirect: 'signin',
-  failureFlash: true
-}));
-
-router.get('/user/profile', function(req, res, next) {
-  res.render('user/profile');
 });
 
 module.exports = router;
