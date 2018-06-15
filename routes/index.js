b138bdf58f7f00b5bc3cef2a95fb27d77b73a5c7
Romain Campech <romain.campech@city.ac.uk>
2018-06-15 01:33:22 +0100

    Added signup view + csrf protection/session packages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var csrf = require('csurf');
+
 var Product = require('../models/product');
+
+var csrfProtection = csrf();
+router.use(csrfProtection);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,6 +17,14 @@ router.get('/', function(req, res, next) {
     }
     res.render('shop/index', { products: productChunks });
   });
+});
+
+router.get('/user/signup', function(req, res, next) {
+  res.render('user/signup', { csrfToken: req.csrfToken() });
+});
+
+router.post('/user/signup', function(req, res, next) {
+  res.redirect('/');
 });
 
 module.exports = router;
