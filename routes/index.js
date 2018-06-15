00f84af556b9176318679c543aca2207f1cb931c
Romain Campech <romain.campech@city.ac.uk>
2018-06-15 16:45:47 +0100

    Added Cart model + add to cart logic

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var Cart = require('../models/cart');
 
 var Product = require('../models/product');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   Product.find(function(err, docs) {
     var productChunks = [];
@@ -12,6 +12,21 @@ router.get('/', function(req, res, next) {
       productChunks.push(docs.slice(i, i + chunkSize))
     }
     res.render('shop/index', { products: productChunks });
+  });
+});
+
+router.get('/add-to-cart/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+  Product.findById(productId, function(err, product) {
+    if (err) {
+      return res.redirect('/');
+    }
+    cart.add(product, product.id);
+    console.log(cart);
+    req.session.cart = cart;
+    res.redirect('/');
   });
 });
 
