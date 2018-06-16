ce87cf094d976da66005db374e51f86bddc4aaa8
Romain Campech <romain.campech@city.ac.uk>
2018-06-16 23:17:47 +0100

    Added client side Stripe credit card validation

--- public/javascripts/checkout.js
+++ public/javascripts/checkout.js
@@ -1,14 +1,41 @@
 var stripe = Stripe('pk_test_7frcV4H48U0ldwUDEnrOihJS');
 var elements = stripe.elements();
 
-var card = elements.create('card');
+var card = elements.create('card', { hidePostalCode: true });
 card.mount('#card-element');
 
 card.addEventListener('change', function(event) {
-  var displayError = document.getElementById('card-errors');
+  var errorElement = document.getElementById('card-errors');
   if (event.error) {
-    displayError.textContent = event.error.message;
+    errorElement.textContent = event.error.message;
   } else {
-    displayError.textContent = '';
+    errorElement.textContent = '';
   }
 });
+
+var form = document.getElementById('checkout-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  var button = form.querySelector('button');
+  button.setAttribute('disabled', 'true');
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+      button.removeAttribute('disabled');
+    } else {
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+function stripeTokenHandler(token) {
+  var hiddenInput = document.createElement('input');
+  hiddenInput.setAttribute('type', 'hidden');
+  hiddenInput.setAttribute('name', 'stripeToken');
+  hiddenInput.setAttribute('value', token.id);
+  form.appendChild(hiddenInput);
+
+  form.submit();
+}
